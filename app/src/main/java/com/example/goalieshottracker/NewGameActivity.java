96c08e14d6783893a18c7a6cac0c4988968b566e
Marlan Govender <mgovender@bitstew.com>
2019-06-04 23:47:01 -0700

    Need to fix main activity thread

--- app/src/main/java/com/example/goalieshottracker/NewGameActivity.java
+++ app/src/main/java/com/example/goalieshottracker/NewGameActivity.java
@@ -1,4 +1,13 @@
 package com.example.goalieshottracker;
 
-class NewGameActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+    }
 }

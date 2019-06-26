17e221aa01bac3d5763e4b0dedd0e4789dda79ed
Marlan Govender <mgovender@bitstew.com>
2019-06-25 21:04:27 -0700

    Fixed single tap by using DrawableImageView

---
+++ app/src/main/java/com/example/goalieshottracker/DrawableImageView.java
@@ -0,0 +1,89 @@
+package com.example.goalieshottracker;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawableImageView extends android.support.v7.widget.AppCompatImageView implements View.OnTouchListener
+{
+    float downx = 0;
+    float downy = 0;
+    float upx = 0;
+    float upy = 0;
+
+    Canvas canvas;
+    Paint paint;
+    Matrix matrix;
+
+    public DrawableImageView(Context context)
+    {
+        super(context);
+        setOnTouchListener(this);
+    }
+
+    public DrawableImageView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        setOnTouchListener(this);
+    }
+
+    public DrawableImageView(Context context, AttributeSet attrs,
+                             int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+        setOnTouchListener(this);
+    }
+
+    public void setNewImage(Bitmap alteredBitmap, Bitmap bmp)
+    {
+        canvas = new Canvas(alteredBitmap );
+        paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setStrokeWidth(10);
+        paint.setColor(Color.BLACK);
+        matrix = new Matrix();
+        canvas.drawBitmap(bmp, matrix, paint);
+
+        setImageBitmap(alteredBitmap);
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event)
+    {
+        int action = event.getAction();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                downx = getPointerCoords(event)[0];//event.getX();
+                downy = getPointerCoords(event)[1];//event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                upx = getPointerCoords(event)[0];//event.getX();
+                upy = getPointerCoords(event)[1];//event.getY();
+                canvas.drawCircle(upx,upy,55 ,paint);
+                invalidate();
+                break;
+        }
+        return true;
+    }
+
+    final float[] getPointerCoords(MotionEvent e)
+    {
+        final int index = e.getActionIndex();
+        final float[] coords = new float[] { e.getX(index), e.getY(index) };
+        Matrix matrix = new Matrix();
+        getImageMatrix().invert(matrix);
+        matrix.postTranslate(getScrollX(), getScrollY());
+        matrix.mapPoints(coords);
+        return coords;
+    }
+}
